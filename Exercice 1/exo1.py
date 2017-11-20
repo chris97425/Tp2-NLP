7a538179761397824bdd8f8dc978fe897d0b891f
christopher <christopher.deboisvilliers@gmail.com>
2017-11-20 11:28:55 -0500

    kazi finish

--- Exercice 1/exo1.py
+++ Exercice 1/exo1.py
@@ -2,8 +2,15 @@
 
 import re , os, codecs
 
-def get_wfb_info():
+#La fonction indexPays permet de creer le dossier index pays
+##################################### Partie 0
+def indexPays(chemin):
+    newpath = r""+chemin+'indexpays'
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
 
+def get_wfb_info():
+##################################### Partie 1
     os.chdir("factbook/geos")
 
     corel = codecs.open("aa.html", "r",'utf-8')
@@ -13,7 +20,7 @@ def get_wfb_info():
 
     listePays = re.findall("geos/(...html)", allcorel)
     del listePays[0]
-
+##################################### Partie 2
     for i in range(0,len(listePays)):
 
         os.chdir("factbook/geos")
@@ -24,12 +31,13 @@ def get_wfb_info():
 
         nompays = re.search("[\w\W]*?xx[\w\W]*?" + listePays[i] + "\"> ?([\w\W]*?) </", pays)
 
+        indexPays("../../")
         os.chdir("../../indexpays/")
 
 
         text_file = open(nompays.group(1) + ".json", "w", encoding="utf8")
         text_file.write("{\"name\":\"" + nompays.group(1) + "\",\n ")
-
+##################################### Partie 3
         try:
 
             introduction = re.search("Introduction[\w\W]*?middle[\w\W]*?a\">(.*?)</", pays)
